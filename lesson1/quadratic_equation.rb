2fcdd2069f6dcf0d8702812366cee536acf4c5bf
lain0 <trampampushe4ka@gmail.com>
2021-05-20 05:39:11 +0300

    feat: lesson1 4/4

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -2,6 +2,9 @@
 # quadratic_equation.rb
 # frozen_string_literal: true
 
+# [1, 7, -12]
+# [1, 4, 4]
+
 class String
   def red;        "\e[31m#{self}\e[0m" end
   def green;      "\e[32m#{self}\e[0m" end
@@ -17,5 +20,15 @@ end
 a = gets_to_f { 'Enter a' }
 b = gets_to_f { 'Enter b' }
 c = gets_to_f { 'Enter c' }
-
-
+discriminant = b**2 - 4 * a * c
+disc_text = "Дискриминант = #{discriminant}"
+if discriminant.negative?
+  puts "#{disc_text}, Корней нет".red
+elsif discriminant.positive?
+  x1 = (-b + Math.sqrt(discriminant)) / (2 * a)
+  x2 = (-b - Math.sqrt(discriminant)) / (2 * a)
+  puts "#{disc_text}, x1 = #{x1}, x2 = #{x2}".green
+elsif discriminant.zero?
+  x = -b / (2 * a)
+  puts "#{disc_text}, x1=x2 = #{x}".blue
+end
