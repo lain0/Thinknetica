21c355fb8ceb5d99e745bd5bc53ae41b2cbcd714
lain0 <trampampushe4ka@gmail.com>
2021-08-03 03:48:43 +0300

    feat: car_passenger OK

--- lesson7/car_passenger.rb
+++ lesson7/car_passenger.rb
@@ -2,14 +2,24 @@
 
 # CarPassenger
 class CarPassenger < Car
-  def initialize(number, seats_total_number)
+  attr_reader :seats
+
+  def initialize(number, seats)
     @type = TYPES[1]
     super(number, @type)
-    @seats_total_number = seats_total_number
-    @seats_occupied_number = 0
+    @seats = seats.to_i
+    @seats_occupied = 0
   end
 
   def take_seat
+    @seats_occupied += 1
+  end
 
+  def seats_free
+    @seats - @seats_occupied
+  end
+
+  def seats_occupied
+    @seats_occupied
   end
 end
--- spec/lesson7/car_passenger_spec.rb
+++ spec/lesson7/car_passenger_spec.rb
@@ -8,15 +8,30 @@ RSpec.describe CarPassenger, type: :model do
   context 'when class attr_reader && initializer works' do
     subject { described_class }
 
+    car = described_class.new('0', 123)
+
     it { is_expected.to eq described_class }
 
-    it 'attr_reader type exists && type works' do
-      car = described_class.new('0')
+    it 'attr_reader type' do
       expect(car.type).to eq(Car::TYPES[1])
     end
-    it "#{described_class}.instance_methods should include methods #take_seat" do
-      expect((described_class.instance_methods - Class.methods).to_set).to be_superset([:take_seat].to_set)
-      # .respond_to?(:take_seat)
+
+    it 'attr_reader seats' do
+      expect(car.seats).to eq(123)
+    end
+
+    it 'include methods: #take_seat #free_seats' do
+      expect((described_class.instance_methods - Class.methods).to_set).to be_superset(%i[take_seat seats_free seats_occupied].to_set)
+      # expect(described_class.instance_methods.respond_to?(:take_seat)).to be_truthy
+    end
+
+    it '#take_seat' do
+      car.take_seat
+      expect(car.seats_occupied).to eq(1)
+    end
+
+    it '#free_seats' do
+      expect(car.seats_free).to eq(122)
     end
   end
 end
